6a46e0279dc50ad35b37d9dd9a2beb199d824af1
Andrew Childre <35944206+AndrewChildre@users.noreply.github.com>
2021-08-26 23:56:39 +0000

    prtotype

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 kaboom({
   global: true,
   fullscreen: true,
-  scale: 0.6,
+  scale: 0.7,
   debug: true,
-  clearColor: [0,0,0,1]
+  clearColor: [0,0,0,0.8]
 })
 
 
@@ -31,51 +31,96 @@ loadSprite('stair', 'VghkL08.png'),
 loadSprite('backgnd', 'u4DVsx6.png',)
 
 scene('game', (
-  // {level, score}
+  {level, score}
   ) => {
 
     layers(['backgnd', 'obj', 'ui'], 'obj')
 
     const map = [
 
-'wtttttttttttttttttx',
-'l                 r',
-'l                 r',
-'l                 r',
-'l                 r',
-'l                 r',
-'l                 r',
-'l                 r',
-'l                 r',
-'ybbbbbbbbbbbbbbbbbz',
+'wtt#t^ttttttttttx',
+'l               r',
+'l   *           r',
+'l               r',
+'l    *        1 r',
+'l     %         r',
+'()              r',
+'l               #',
+'l               r',
+'ybbbbbbbbbbbbbbbz',
 
     ]
    const levelConfig = {
      width: 48,
      height: 48,
-    'l' : [sprite('left-wall')],
-    'r' : [sprite('right-wall')],
-    't' : [sprite('top-wall')],
-    'b' : [sprite('bottom-wall')],
-    'w' : [sprite('top-left-cnr')],
-    'x' : [sprite('top-right-cnr')],
-    'y' : [sprite('btm-left-cnr')],
-    'z' : [sprite('btm-right-cnr')],
+    'l' : [sprite('left-wall'), solid(), 'wall'],
+    'r' : [sprite('right-wall'), solid(), 'wall'],
+    't' : [sprite('top-wall'), solid(), 'wall'],
+    'b' : [sprite('bottom-wall'), solid(), 'wall'],
+    'w' : [sprite('top-left-cnr'), solid(), 'wall'],
+    'x' : [sprite('top-right-cnr'), solid(), 'wall'],
+    'y' : [sprite('btm-left-cnr'), solid(), 'wall'],
+    'z' : [sprite('btm-right-cnr'), solid(), 'wall'],
     '(' : [sprite('left-door')],
     '^' : [sprite('top-door')],
     '?' : [sprite('stair')],
-    '*' : [sprite('slicer')],
-    '1' : [sprite('bad-guy')],
-    '#' : [sprite('lantern')],
-    '%' : [sprite('fire-can')],
+    '*' : [sprite('slicer'), 'slicer', 'dangerous',{ dir: -1}],
+    '1' : [sprite('bad-guy'), 'dangerous'],
+    '#' : [sprite('lantern'), solid(), 'wall'],
+    '%' : [sprite('fire-can'), solid()],
 
    }
 
     addLevel(map, levelConfig)
     // add([sprite('backgnd'), layer('backgnd')])
+    const scoreLabel = add([
+      scale(4),
+      text('0'),
+      pos(400,500),
+      layer('ui'),
+      {
+        value: score,
+      }
+    ])
+   const player = add([sprite('guy-right'), pos(10, 290)])
+
+   player.action(() => {
+     player.resolve()
+   })
+
+    const moveSpeed = 120
+   keyDown('left', () => {
+     player.changeSprite('guy-left')
+     player.move(-moveSpeed, 0)
+   })
+    keyDown('right', () => {
+      player.changeSprite('guy-right')
+     player.move(moveSpeed, 0)
+   })
+    keyDown('up', () => {
+      player.changeSprite('guy-up')
+     player.move(0, -moveSpeed)
+   })
+    keyDown('down', () => {
+      player.changeSprite('guy-down')
+     player.move(0, moveSpeed)
+   })
+    const slicerSpeed = 120
+   action('slicer', (s) => {
+      s.move(s.dir * slicerSpeed, 0) 
+   })
+   collides('slicer', 'wall', (s) => {
+     s.dir = -s.dir
+   })
+   player.overlaps('dangerous', () => {
+     go('lose', {score: scoreLabel.value})
+   })
+   scene('lose', ({ score }) => {
+      add([text(score, 32), origin('center'), pos(width() /2, height() /2)])
+   })
 })
 start("game", 
-// {level: 0, score: 0 }
+ {level: 0, score: 0 }
 )
 
 
